dd3a37d6cd9679c33d6f710067c09bbedc143192
dimitrislysandros <dimitrislysandros@gmail.com>
2025-12-29 19:18:43 +0200

    updated visuals

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-bio',
    standalone: true,     
-  imports: [],
+  imports: [NgbCollapse],
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.scss'
 })
 export class BioComponent {
 
+	public isCollapsed = false;
 }
